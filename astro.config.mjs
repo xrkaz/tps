aa4bdb8f784229b69fed13ffb08a979077e3be9d
xrkaz <65538621+xrkaz@users.noreply.github.com>
2023-08-14 22:03:36 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://xrkaz.github.io',
-  // base: '/tpspn',
+  site: 'https://xrkaz.github.io',
+  base: '/tps',
 });
